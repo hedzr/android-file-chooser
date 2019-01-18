a4d21046ec24323d16aca95428cbbe7ef8376a8e
Hedzr Yeh <hedzrz@gmail.com>
2019-01-18 19:56:42 +0800

    bumped version to v1.1.12

--- library/src/main/java/com/obsez/android/lib/filechooser/Versions.java
+++ library/src/main/java/com/obsez/android/lib/filechooser/Versions.java
@@ -4,5 +4,5 @@ package com.obsez.android.lib.filechooser;
  * Created by coco on 6/7/15.
  */
 public final class Versions {
-    public final static int VERSION_CODE = 101011;
+    public final static int VERSION_CODE = 101012;
 }
